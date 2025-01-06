21741f02bdd26bd8bfeaac203bf46a683d66c71f
aakashchothani0311 <aakashchothani@gmail.com>
2025-01-06 16:00:24 -0500

    HeroSection icon alignment for mobile devices

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -42,7 +42,7 @@ export const HeroSection = () => {
                     <p className='mb-6 text-[#ADB7BE] text-base sm:text-lg lg:text-xl'>
                         Welcome to my portfolio.
                     </p>
-                    <div className='mb-6 flex flex-row place-self-center lg:justify-self-start gap-6'>
+                    <div className='mb-6 flex flex-row justify-self-center lg:justify-self-start gap-6'>
                         <Link href='mailto:aakashchothani@gmail.com'>
                             <EnvelopeIcon className='h-8 w-8 sm:h-12 sm:w-12 text-white'/>
                         </Link>
