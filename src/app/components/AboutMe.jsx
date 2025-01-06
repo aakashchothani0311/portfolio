366010223aaff7cab70752af22fcd167014fe0a9
aakashchothani0311 <aakashchothani@gmail.com>
2025-01-06 15:43:58 -0500

    Updated About Me text

--- src/app/components/AboutMe.jsx
+++ src/app/components/AboutMe.jsx
@@ -11,11 +11,20 @@ export const AboutMe = () => {
     return (
         <motion.section id="about" ref={ref} className='lg:py-8' style={{ y, opacity }}>
             <h2 className='mt-4 mb-8 text-center text-4xl font-bold text-white'>About Me</h2>
-            <p className='text-white lg:text-lg'>
-                I am Aakash Chothani. I am currently pursuing my Masters in Software Engineering Systems from Northeastern University - Boston campus.
-                <br/><br/>
-                As I prepare for internships/ co-op opportunities starting January 2025, I am seeking roles in Software Engineering/ Full Stack development or as a Salesforce Developer. With three years of professional experience as a Full Stack Developer, I possess skills in Java, HTML, CSS and JavaScript along with other widely used web technologies like the MERN stack, Node.js, etc. My foundation of web technologies enables me to easily adapt to new frameworks.
-            </p>
+            <div className='text-white lg:text-lg text-justify space-y-4'>
+                <p>
+                    Greetings. I’m Aakash Chothani, a results-driven Full Stack Engineer with a strong foundation in software engineering, currently pursuing Master's degree in Software Engineering Systems from Northeastern University, Boston.
+                </p>
+                <p>
+                    Over three years of professional experience at Accenture, I led projects focused on Salesforce integration, web component development, and data-driven applications. With a diverse technical skill set ranging from Java, JavaScript, TypeScript, MySQL, MongoDB, to modern web development frameworks like React, Node.js, MUI, Tailwind CSS. My foundation of web technologies enables me to easily adapt to new frameworks. Additionally, I am 3x Salesforce Certified.
+                </p>
+                <p>
+                    I believe in continuous learning and am currently excelling in my Master’s program while contributing as a Graduate Teaching Assistant for the course - INFO 6205: Program Structures and Algorithms.
+                </p>
+                <p>
+                    Please feel free to reach out and connect with me to discuss about potential opportunities in the role of Software Engineering/ Full Stack development/ Salesforce Developer.
+                </p>
+            </div>
         </motion.section>
     );
 }
